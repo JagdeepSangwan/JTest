b862927b6ebd1db3d9fff3a14caacf5feeaeb497
Jagdeep Sangwan <jagdeep@clavrit.com>
2021-10-14 20:31:03 +0530

    edited message

--- src/com/jss/JssMain.java
+++ src/com/jss/JssMain.java
@@ -4,6 +4,6 @@ public class JssMain {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Hello Jagdeep");
+        System.out.println("Hello Jagdeep Sangwan");
     }
 }
