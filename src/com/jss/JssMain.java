5e3a2af3e022563cf0d3bf433365a3bfe74b6a98
Jagdeep Sangwan <jagdeep@clavrit.com>
2021-10-14 20:27:53 +0530

    worked on feature 1.0

--- src/com/jss/JssMain.java
+++ src/com/jss/JssMain.java
@@ -5,5 +5,10 @@ public class JssMain {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Jagdeep");
+        System.out.println("Sum is: "+add(3,5));
+    }
+
+    public int add(int a, int b){
+        return a+b;
     }
 }
