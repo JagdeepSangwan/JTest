fa82438c720f9aa00bf0a7584348bce9b0ee242c
Jagdeep Sangwan <jagdeep@clavrit.com>
2021-10-14 20:41:32 +0530

    add variables

--- src/com/jss/JssMain.java
+++ src/com/jss/JssMain.java
@@ -4,11 +4,13 @@ public class JssMain {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 4;
+        int b = 5;
         System.out.println("Hello Jagdeep");
         System.out.println("Sum is: "+add(3,5));
     }
 
-    public int add(int a, int b){
+    public static int add(int a, int b){
         return a+b;
     }
 }
