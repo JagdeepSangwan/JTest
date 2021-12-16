7f0bda91bf013096f36e8dd8ce938f8dfc8f5e75
Jagdeep Sangwan <jagdeep@clavrit.com>
2021-12-16 17:08:24 +0530

    Added Simple Encoding and Decoding for String

--- src/com/jss/JssMain.java
+++ src/com/jss/JssMain.java
@@ -1,16 +1,46 @@
 package com.jss;
 
+import java.util.logging.Logger;
+
 public class JssMain {
+    public static final Logger LOG = Logger.getLogger("JssMain");
 
     public static void main(String[] args) {
-	// write your code here
         int a = 4;
-        int b = 5;
-        System.out.println("Hello Jagdeep");
-        System.out.println("Sum is: "+add(3,5));
+        int b = 249;
+        String email = "xyz@gmail.com";
+        String emailEncoded = encodeEmail(email);
+        String emailDecoded = decodeEmail(emailEncoded);
+        printLog("Hello Jagdeep");
+        printLog("Sum is: "+add(a, b));
+        printLog("Encoded email: "+emailEncoded);
+        printLog("Decoded email: "+emailDecoded);
     }
 
     public static int add(int a, int b){
         return a+b;
     }
+
+    public static String encodeEmail(String email){
+        char[] emailArray = email.toCharArray();
+        char[] emailEncoded = new char[email.length()];
+        for(int i=0;i<email.length();i++){
+            emailEncoded[i] = (char) (emailArray[i]+10);
+        }
+        return String.valueOf(emailEncoded);
+    }
+
+    public static String decodeEmail(String emailEncoded) {
+        char[] emailArray = emailEncoded.toCharArray();
+        char[] emailDecoded = new char[emailEncoded.length()];
+        for(int i=0;i<emailEncoded.length();i++){
+            emailDecoded[i] = (char) (emailArray[i]-10);
+        }
+        return String.valueOf(emailDecoded);
+    }
+
+    public static void printLog(String str) {
+        LOG.info(str);
+    }
+
 }
